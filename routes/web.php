852d8db47f02007803fa00336f5bf6c01ec7c31d
jonatasmcampos <camposjonatas3@gmail.com>
2022-06-22 17:13:47 -0300

    front end pagina inicial

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index']);
+
 Auth::routes();
 
 //grupo de rotas para admin
